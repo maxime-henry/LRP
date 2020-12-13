40bbbe5d4ddb1577d365e60dea25bd47abaf43dc
Maxime HENRY <maxime.3497@gmail.com>
2020-12-13 14:01:29 +0100

    Update lrp.R

--- R/lrp.R
+++ R/lrp.R
@@ -18,8 +18,8 @@
 lrp<-function(relevance,resuh){
   relevance %>% ggplot(aes(y=as.numeric(names))) + geom_point(aes(x=V1-V1,color=-V1,size=6),stat='identity',alpha=0.7,show.legend = FALSE)+
   theme_minimal()+
-    theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.y = element_blank())+
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+    theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.text.y=element_blank(),axis.ticks.x=element_blank(),axis.title.y = element_blank())+
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),axis.ticks.y =element_blank())+
     guides(fill=FALSE)+scale_y_continuous(trans = "reverse")+
     geom_point(aes(x=0.8,y=(nrow(relevance)/2)),size=6)+xlim(-1,1)+
     annotate(geom='text',x=0.85,y=(nrow(relevance)/2)+2.5,label=paste('Relevance = ',round(sum(relevance$V1),1)))->legraph
@@ -27,22 +27,29 @@ lrp<-function(relevance,resuh){
 relevance %>% mutate(alpha=rescale(V1))->certainesfleches
 
 for(i in 1:nrow(relevance)){
+  legraph<-legraph+annotate(geom='text',x=0,y=i,label=i,size=3,color='white')
   if(i %in% certainesfleches$names == TRUE){
     legraph<-legraph+annotate(geom='segment',x = 0.7, y =(nrow(relevance)/2) , yend = i, xend = 0.05,alpha=certainesfleches[i,3],size=1, arrow = arrow(length = unit(0.3, "cm")))
   }
 }
-legraph<-legraph+labs(title="Distribution de la relevance a travers une couche cachee")
-
-Relevance<-as.data.frame(rowSums(resuh))
-Relevance %>% rename(Somme='rowSums(resuh)') %>% mutate(var=rownames(Relevance))  %>% slice_max(order_by=Somme,n=10) %>% mutate(alpha=rescale(Somme))->variables
+legraph<-legraph+labs(title="Distribution de la relevance a travers une couche cachee",subtitle = 'Representation des variables les plus importantes par neurones')
 
 
 
-for(i in 1:nrow(variables)){
+alph<-matrix(ncol=1,nrow=nrow(resuh1))
 
-  legraph<-legraph+annotate(geom='text',x=-0.8,y=i+5,label=variables[i,2])
+for(i in 1:nrow(resuh1)){
+as.data.frame(resuh) %>% mutate(var=rownames(resuh)) %>% slice_max(order_by = resuh[,i],n=1) ->variablelocale
+  legraph<-legraph+annotate(geom='text',x=-0.8,y=i,label=variablelocale[1,'var'])
 
+alph[i,1]=variablelocale[1,i]
 
+}
+
+alph[,1]<-rescale(alph[,1])
+
+for(i in 1:nrow(resuh1)){
+legraph<-legraph+annotate(geom='segment',x = -0.1, y =i , yend = i, xend = -0.5,alpha=alph[i,1],size=1, arrow = arrow(length = unit(0.3, "cm")))
 }
 
 return(legraph)
